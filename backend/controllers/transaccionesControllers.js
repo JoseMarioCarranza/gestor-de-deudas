f144828c274dff355bdebd32f376f97296dc3334
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-11-19 15:26:41 -0700

    Rutas, controladores, errorHandler y async

--- backend/controllers/transaccionesControllers.js
+++ backend/controllers/transaccionesControllers.js
@@ -1,18 +1,24 @@
-const getTransacciones = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTransacciones = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Obtener transacciones' })
-}
+})
 
-const crearTransaccion = (req, res) => {
+const crearTransaccion = asyncHandler(async (req, res) => {
+    if (!req.body.tipo || !req.body.cantidad) {
+        res.status(400)
+        throw new Error("Por favor teclea una descripción")
+    }
     res.status(201).json({ message: 'Se creo transaccion' })
-}
+})
 
-const modificarTransaccion = (req, res) => {
+const modificarTransaccion = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Se modifico transaccion ${req.params.id}` })
-}
+})
 
-const eliminarTransaccion = (req, res) => {
+const eliminarTransaccion = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Se elimino transacciones ${req.params.id}` })
-}
+})
 
 module.exports = {
     getTransacciones,
